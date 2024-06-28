2943e0542fc3ecb80debf476606dff9f20d8e075
Sasha Bogolii <sbogolii@siftscience.com>
2024-06-28 16:52:23 +0300

    Added Task

--- src/domain.js
+++ src/domain.js
@@ -1,34 +1,41 @@
-﻿export default class TeamMember {
-    #workHours = []; // UTC
+﻿export class TeamMember {
+    workHours = []; // UTC
     static DAYS_IN_TWO_SPRINTS = 20;
     static START_HOUR = 9;
 
     constructor(accountId, timeZone) {
         this.accountId = accountId;
         this.timeZone = timeZone;
+        this.initWorkHours();
+    }
+
+    initWorkHours() {
         let options = {
             timeZone: this.timeZone,
             hour: '2-digit',
             hour12: false
         };
         let formatter = new Intl.DateTimeFormat([], options);
-
-
         for (let day = 0; day < TeamMember.DAYS_IN_TWO_SPRINTS; day++) {
             let currentTime = new Date();
             currentTime.setUTCHours(0, 0, 0, 0);
             currentTime.setUTCMilliseconds(currentTime.getUTCMilliseconds() + day * 24 * 60 * 60 * 1000);
-            console.log(`Day: ${currentTime}`);
+            //console.log(`Day: ${currentTime}`);
             for (let hourOfDayUTC = 0; hourOfDayUTC < 24; hourOfDayUTC++) {
                 currentTime.setUTCHours(hourOfDayUTC, 0, 0, 0);
-                
                 let hourInTimeZone = formatter.format(currentTime);
-                this.#workHours[hourOfDayUTC] = hourInTimeZone >= TeamMember.START_HOUR && hourInTimeZone < (TeamMember.START_HOUR + 4) ||
-                hourInTimeZone >= (TeamMember.START_HOUR + 5) && hourInTimeZone < (TeamMember.START_HOUR + 9);
-
-                console.log(`User: ${accountId} in ${timeZone} timeZone. 
-                    At UTC hour ${hourOfDayUTC}, local hour ${hourInTimeZone}, working=${this.#workHours[hourOfDayUTC]}`);
+                this.workHours[hourOfDayUTC] = hourInTimeZone >= TeamMember.START_HOUR && hourInTimeZone < (TeamMember.START_HOUR + 4) ||
+                    hourInTimeZone >= (TeamMember.START_HOUR + 5) && hourInTimeZone < (TeamMember.START_HOUR + 9);
+                //console.log(`User: ${accountId} in ${timeZone} timeZone. 
+                //    At UTC hour ${hourOfDayUTC}, local hour ${hourInTimeZone}, working=${this.#workHours[hourOfDayUTC]}`);
             }
         }
+    }
+}
+
+export class Task {
+    constructor(id, estimatedHours) {
+        this.id = id;
+        this.estimatedHours = estimatedHours;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Necessary import for calling Jira REST APIs
 import api, { route } from "@forge/api";
-import TeamMember from "./domain";
+import { TeamMember, Task } from "./domain";
 
 const buildOutput = () => ({
   headers: {
@@ -23,12 +23,25 @@ const getUser = async (accountId) => {
     }
   });
 
-  console.log(`Response: ${response.status} ${response.statusText}`);
+  //console.log(`Response: ${response.status} ${response.statusText}`);
 
   const user = await response.json();
-  console.log(user);
+  //console.log(user);
 
   return user;
+}
+
+const getIssue = async (issueIdOrKey) => {
+  const response = await api.asApp().requestJira(route`/rest/api/2/issue/${issueIdOrKey}`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  //console.log(`Response: ${response.status} ${response.statusText}`);
+  const issue = await response.json();
+  //console.log(issue);
+  return issue;
 }
 
 
@@ -46,12 +59,12 @@ const assignIssueToUser = async (issueIdOrKey, userId) => {
     body: bodyData
   });
 
-  console.log(`Response: ${response.status} ${response.statusText}`);
+  //console.log(`Response: ${response.status} ${response.statusText}`);
 }
 
 export const run = async (req) => {
   // This is the request from A4J
-  console.log(req);
+  //console.log(req);
 
   // Parses the request body from A4J as it is a string when we got it
   const parsedBody = JSON.parse(req.body);
@@ -63,21 +76,27 @@ export const run = async (req) => {
   // If you decided to add Authorization header in the A4J request
   // This is a basic handling of that token
   if (authToken != 'Bearer my-token') {
-    console.log('Invalid token');
+    //console.log('Invalid token');
     return {
       statusCode: 401,
       statusText: 'Unauthorized'
     };
   }
 
-  console.log('Correct token');
+  //console.log('Correct token');
 
   // Calling the GET sprint API passing a parameter from the A4J action 
   // You can change the API depending on your use case
   const issuesResponsePromise = await api.asApp()
     .requestJira(route`/rest/agile/1.0/sprint/${parsedBody.sprint_id}/issue`);
   const issuesResponse = await issuesResponsePromise.json();
-  console.log(issuesResponse);
+  const issues = await Promise.all(issuesResponse.issues
+    .map(async (issue) => await getIssue(issue.key)));
+
+  console.log(issues);
+
+  const tasks = issues.map(issue =>  new Task(issue.key, issue.fields.customfield_10016));
+  console.log(tasks);
 
   const usersResponsePromise = await api.asApp().requestJira(route`/rest/api/2/users/search`);
   const allUsers = await usersResponsePromise.json();
@@ -87,7 +106,7 @@ export const run = async (req) => {
 
   const teamMembers = activeUsers.map(user => new TeamMember(user.accountId, user.timeZone));
 
-  console.log(teamMembers);
+  //console.log(teamMembers);
 
   issuesResponse.issues.forEach(issue => {
     assignIssueToUser(issue.key, getRandomObject(activeUsers).accountId);
